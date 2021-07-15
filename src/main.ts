60ebe53155f0ba0adf6daa5c0908fecae555be28
david <dcrbrown@outlook.com>
2021-07-15 19:43:10 +0100

    Getting started

---
+++ src/main.ts
@@ -0,0 +1,178 @@
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, TextComponent } from 'obsidian';
+
+type Macro = {
+	text: string;
+}
+
+interface PluginSettings {
+	macros: Macro[];
+}
+
+const DEFAULT_SETTINGS: PluginSettings = {
+	macros: [],
+}
+
+export default class MacroPlugin extends Plugin {
+	settings: PluginSettings;
+
+	async onload() {
+		console.log('loading plugin');
+
+		await this.loadSettings();
+
+		// this.addRibbonIcon('dice', 'Sample Plugin', () => {
+		// 	new Notice('This is a notice!');
+		// });
+
+		// this.addStatusBarItem().setText('Status Bar Text');
+
+		this.addCommand({
+			id: 'macro',
+			name: 'Manage Macros',
+			// callback: () => {
+			// 	console.log('Simple Callback');
+			// },
+			checkCallback: (checking: boolean) => {
+				let leaf = this.app.workspace.activeLeaf;
+				if (leaf) {
+					if (!checking) {
+						new MacroModal(this.app, this, this.settings).open();
+					}
+					return true;
+				}
+				return false;
+			}
+		});
+
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
+
+		this.registerCodeMirror((cm: CodeMirror.Editor) => {
+			console.log('codemirror', cm);
+		});
+
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	onunload() {
+		console.log('unloading plugin');
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class MacroModal extends Modal {
+	settings: PluginSettings;
+	plugin: MacroPlugin;
+
+	constructor(app: App, plugin: MacroPlugin, settings: PluginSettings) {
+		super(app);
+		this.settings = settings;
+		this.plugin = plugin;
+	}
+
+	render() {
+		const {contentEl} = this;
+		while (contentEl.firstChild) {
+			contentEl.removeChild(contentEl.firstChild);
+		}
+
+		contentEl.createEl('h2', {text: 'Macros'});
+
+		console.log('rendering macros', this.settings.macros);
+
+		this.settings.macros.forEach(macro => {
+			const container = contentEl.createDiv();
+			const input = contentEl.createEl('input', {
+				type: 'text',
+			});
+			input.value = macro.text;
+
+			const confirm = contentEl.createEl('button', {
+				type: 'button',
+				text: 'Confirm',
+			});
+			confirm.addEventListener('click', () => {
+				console.log('confirming changes', input.value);
+				macro.text = input.value;
+				this.updateMacros();
+			});
+
+			const deleteMacro = contentEl.createEl('button', {
+				type: 'button',
+				text: 'Delete',
+			});
+			deleteMacro.addEventListener('click', () => {
+				console.log('deleting macro');
+				this.settings.macros = this.settings.macros.filter(m => m !== macro);
+				this.updateMacros();
+			});
+
+			container.appendChild(input);
+			container.appendChild(confirm);
+			container.appendChild(deleteMacro);
+		});
+
+		const controls = contentEl.createDiv();
+		const addNew = contentEl.createEl('button', {
+			type: 'button',
+			text: 'Add new macro',
+		});
+		addNew.addEventListener('click', () => {
+			console.log('adding new macro');
+			this.settings.macros.push({
+				text: 'test',
+			});
+			this.updateMacros();
+		});
+		controls.appendChild(addNew);
+	}
+
+	async updateMacros() {
+		this.plugin.saveSettings();
+		this.render();
+	}
+
+	onOpen() {
+		this.render();
+	}
+
+	onClose() {
+		let {contentEl} = this;
+		contentEl.empty();
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MacroPlugin;
+
+	constructor(app: App, plugin: MacroPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let {containerEl} = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h2', {text: 'Settings for Obsidian Macros'});
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue('')
+				.onChange(async (value) => {
+					console.log('Secret: ' + value);
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
